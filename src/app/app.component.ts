7d948ef70d8264df207ac29b1ee97e710020c121
winnemucca <skauyedauty@gmail.com>
2017-05-05 16:53:20 -0600

    more details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { ModelsService } from './models.service';
 import { Model } from './models';
+import { MdSidenav} from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   models: Model[] = [];
+  selectedModel: Model;
+  @ViewChild('sidenav') sidenav: MdSidenav;
+
 
   constructor(private modelsService: ModelsService) {}
 
   ngOnInit(){
     this.models = this.modelsService.getAll();
+  }
+
+  showDetails(model: Model){
+    // 1. set selected models
+    this.selectedModel = model;
+
+    // 2. open sidenav
+    this.sidenav.open();
   }
 }
