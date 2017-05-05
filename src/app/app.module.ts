9d2657a2f39e59b6cdf16a4aacd3709107a367f2
winnemucca <skauyedauty@gmail.com>
2017-05-05 15:41:46 -0600

    cool grid for material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { ModelsService} from './models.service';
+import {
+  MdButtonModule,
+  MdCheckboxModule,
+  MdCardModule,
+  MdGridListModule,
+  MdInputModule,
+  MdToolbarModule,
+  MdSidenavModule,
+  MdTabsModule
+} from '@angular/material';
+
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -11,10 +28,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdCardModule,
+    MdGridListModule,
+    MdInputModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdTabsModule,
+    FlexLayoutModule
+
   ],
-  providers: [],
+  providers: [
+    ModelsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
