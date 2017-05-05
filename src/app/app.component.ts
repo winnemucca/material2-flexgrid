2d650a6e9c9f0ef93404e3f21d59a9f323b7e901
winnemucca <skauyedauty@gmail.com>
2017-05-05 16:27:38 -0600

    images working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { ModelsService } from './models.service';
+import { Model } from './models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  models: Model[] = [];
+
+  constructor(private modelsService: ModelsService) {}
+
+  ngOnInit(){
+    this.models = this.modelsService.getAll();
+  }
 }
